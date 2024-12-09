3628fa2b5a577c0d662390e94bcc9f2d0bbd42b7
Nishant Minerva <nishantcafe@gmail.com>
2024-12-09 10:34:00 +0530

    Initial Commit

---
+++ SkarvoStocks/SkarvoStocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkarvoStocksApp.swift
+//  SkarvoStocks
+//
+//  Created by Nishant Minerva on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SkarvoStocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
